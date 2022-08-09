1dd85d1a7c11f20a41b836fd86d118bcbae595a4
pdienstmann <110914538+pdienstmann@users.noreply.github.com>
2022-08-09 19:33:19 -0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,23 @@
+import { valida } from './validacao.js'
+
+const inputs = document.querySelectorAll('input')
+const args = {
+    prefix: 'R$ ',
+    fixed: true,
+    fractionDigits: 2,
+    decimalSeparator: ',',
+    thousandsSeparator: '.',
+    cursor: 'move'
+  };
+
+inputs.forEach(input => {
+
+    if(input.dataset.tipo === 'preco') {
+        SimpleMaskMoney.setMask(input, args)
+
+    }
+
+    input.addEventListener('blur', (evento) => {
+        valida(evento.target)
+    })
+})
